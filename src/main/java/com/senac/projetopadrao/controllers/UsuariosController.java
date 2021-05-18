4c60f3f4d1cf5318516e3e310a3b70e6b3ad9fcf
Alfrehan <a.hannzel@gmail.com>
2021-05-17 21:56:33 -0300

    Criação da tabela no HTML.

--- src/main/java/com/senac/projetopadrao/controllers/UsuariosController.java
+++ src/main/java/com/senac/projetopadrao/controllers/UsuariosController.java
@@ -1,21 +1,35 @@
 package com.senac.projetopadrao.controllers;
 
 import com.senac.projetopadrao.model.Usuario;
+import com.senac.projetopadrao.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UsuariosController {
 
-    @GetMapping("/listar")
-    public String listarUsuarios(){
+    @Autowired
+    UsuarioRepository usuarioRepository;
+    private Object FindAll;
 
-        System.out.println("USUARIOS");
+    @GetMapping("/")
+    public ModelAndView listarUsuarios(){
+        ModelAndView mv = new ModelAndView( "usuarios");
 
-        return "usuarios";
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+
+        usuarios = (ArrayList<Usuario>) usuarioRepository.findAll();
+
+        mv.addObject("usuarios",usuarios);
+
+        return mv;
     }
 
 }
