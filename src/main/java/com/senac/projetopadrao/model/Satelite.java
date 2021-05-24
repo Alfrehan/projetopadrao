24a84b933d5f775e439c87ed9031fc4d13138659
Alfrehan <a.hannzel@gmail.com>
2021-05-24 17:24:37 -0300

    Criação da atividade 12 - Satelite

---
+++ src/main/java/com/senac/projetopadrao/model/Satelite.java
@@ -0,0 +1,23 @@
+package com.senac.projetopadrao.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Satelite implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idSatelite;
+
+    public int getIdSatelite() {
+        return idSatelite;
+    }
+
+    public void setIdSatelite(int idSatelite) {
+        this.idSatelite = idSatelite;
+    }
+}
